cf369ae66707c5b0d58561c31407fe34c882f4d0
Ash <ash.m10@aluno.ifsc.edu.br>
2023-08-22 16:23:17 -0300

    console.log

--- day2/exercicio.js
+++ day2/exercicio.js
@@ -1,7 +1,15 @@
 let challenge = '30 Days Of JavaScript'
+let phrase = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
 console.log(challenge)
 
 console.log(challenge.length)
 console.log(challenge.toUpperCase())
 console.log(challenge.toLowerCase())
 console.log(challenge.substring(3,21))
+console.log(challenge.substring(0,2))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+console.log(phrase.split(', '))
+console.log(challenge.replace('JavaScript','Python'))
